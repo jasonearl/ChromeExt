7cf3cfe88fda0b7572ee12e009e86d2e5a6637f5
Jason Earl <jasonearl@Jasons-MacBook-Air.local>
2022-05-28 12:39:36 -0700

    Links are saved after closing Chrome, need to improve CSS layout

--- index.js
+++ index.js
@@ -2,29 +2,59 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    // Save the myLeads array to localStorage
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    // PS: remember JSON.stringify()
-    renderLeads()
 
-    // To verify that it works:
-    console.log( localStorage.getItem("myLeads") )
+// this will display the list of leads in the extension
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+// save the tab the is currently open in chrome to list of leads
+tabBtn.addEventListener("click", function(){
+    // in Chrome, get access to the tabs in the current window
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        // save the current tab url to the list of leads
+        myLeads.push(tabs[0].url)
+        // saving to local storage will prevent the tabs from being lost
+        // when Chrome is closed closed
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
-function renderLeads() {
+
+// display the leads in the extension
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems
 }
+
+// remove all of the stored leads
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+// save the user input to the leads list
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    // saving to local storage will prevent the tabs from being lost
+    // when Chrome is closed closed
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
